3530df6691dd09d4ce14f65ddd2fedf61baca428
Allan Wong <alw040@ucsd.edu>
2016-09-20 21:59:49 -0700

    Basic first file commit to test

---
+++ basic.py
@@ -0,0 +1,15 @@
+#Basic program problems solved in python for basic practice. Heavily commented for easy to read step by step
+
+#PROBLEM: Print odds from 1 to 20:
+
+#Use for loop range, if and modulus to print out the numbers
+
+#Use for to loop through list from 1 to 21
+
+for i in range(1, 21):
+
+#use modulus to check if number is odd
+#Proceed to print odd.
+    if ((i % 2) != 0):
+        print ("i = ", i)
+    
