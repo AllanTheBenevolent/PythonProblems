901a85d1ec27c9cc1fc59b7edf8a53531d0515dc
Allan Wong <alw040@ucsd.edu>
2016-09-21 23:19:54 -0700

    Starting function problems

---
+++ function.py
@@ -0,0 +1,5 @@
+# ---------- PROBLEM : SOLVE FOR X ----------
+# Make a function that receives an algebraic equation like
+# x + 4 = 9 and solve for x
+# x will always be the 1st value received and you only
+# will deal with addition
